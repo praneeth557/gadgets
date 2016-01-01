40e7455d5601f81b87177abbaeb9fa4a86eaea94
Praneeth Veerlapally <praneeth7516@gmail.com>
2015-12-31 23:39:32 -0800

    moved files to public folder

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-	res.send('Gadgets application started!!!');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function (){
 	console.log('Express listening on port : ' + PORT);
